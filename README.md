09f5499fe081279b7059059f841326118080aa45
Michael Daniels <mdaniels5757@gmail.com>
2024-12-21 21:59:49 -0500

    update with actual production code (ugly!)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # MDanielsBot-MarkAdminsListUpdater
-Updates data for the enwikiMarkAdmins user script on the English WIkipedia. See https://en.wikipedia.org/wiki/User:Mdaniels5757/markAdmins and https://en.wikipedia.org/wiki/User:MDanielsBot for more.
+Updates data for the enwikiMarkAdmins user script on the English Wikipedia. See https://en.wikipedia.org/wiki/User:Mdaniels5757/markAdmins and https://en.wikipedia.org/wiki/User:MDanielsBot for more.
--- UpdateGroupJson.py
+++ UpdateGroupJson.py
@@ -19,79 +19,66 @@ import pywikibot
 import json
 from datetime import datetime, timezone
 
-site = pywikibot.Site('en', 'wikipedia')
-metawiki = pywikibot.Site('meta', 'meta')
+site = pywikibot.Site('en', 'wikipedia');
+metawiki = pywikibot.Site('meta', 'meta');
 
-
-def globalallusers(site, group):
-    """
-
-    :param site: The site to get our data from.
-    :type site: pywikibot.site.APISite
-    :param group: The group to get.
-    :type group: str
-    :return: A pywikibot generator of all global users in the given global group.
-    :rtype: pywikibot.data.api.ListGenerator
-    """
-    agugen = pywikibot.data.api.ListGenerator('globalallusers',
-                                              aguprop='groups', site=site)
+def globalallusers(site,group):
+    agugen = pywikibot.data.api.ListGenerator('globalallusers',\
+                               aguprop='groups', site=site)
     if group:
         agugen.request['agugroup'] = group
     return agugen
 
-
-def sortkeys(key: str) -> str:
-    """
-    Sorts the user groups before saving them to the wiki.
-
-    @param key: User right
-    @type key: str
-    @return: Corresponding abbreviation
-    @rtype: str
-    """
+def sortkeys(key):
     sortkeyDict = {
-        "sysop": "A",
-        "arbcom": "ARB",
-        "bureaucrat": "B",
-        "checkuser": "CU",
-        "oversight": "CV",
-        "interface-admin": "IA",
-        "abusefilter": "EFM",
-        "abusefilter-helper": "EFH",
-        "accountcreator": "ACC",
-        "autoreviewer": "AP",
-        "extendedmover": "PM",
-        "filemover": "FM",
-        "massmessage-sender": "MMS",
-        "patroller": "NPR",
-        "reviewer": "PCR",
-        "rollbacker": "RB",
-        "global-renamer": "GRe",
-        "global-rollbacker": "GRb",
-        "templateeditor": "TE",
-        "otrs-member": "OTRS",
-        "steward": "S"
+        "sysop" : "A",
+        "arbcom" : "ARB",
+        "bureaucrat" : "B",
+        "checkuser" : "CU",
+        "suppress" : "CV",
+        "interface-admin" : "IA",
+        "abusefilter" : "EFM",
+        "abusefilter-helper" : "EFH",
+        "accountcreator" : "ACC",
+        "autoreviewer" : "AP",
+        "extendedmover" : "PM",
+        "filemover" : "FM",
+        "massmessage-sender" : "MMS",
+        "patroller" : "NPR",
+        "reviewer" : "PCR",
+        "rollbacker" : "RB",
+        "global-renamer" : "GRe",
+        "global-rollbacker" : "GRb",
+        "templateeditor" : "TE",
+#        "otrs-member" : "OTRS",
+	"vrt-permissions" : "VRT",
+        "steward" : "S"
     }
     return sortkeyDict[key]
 
+combinedJsDataPage = pywikibot.Page(site,
+                         "User:MDanielsBot/markAdmins-Data.js")
+combinedJsonDataPage = pywikibot.Page(site,\
+                          "User:MDanielsBot/markAdmins-Data.json")
 
-combinedJsDataPage = pywikibot.Page(site, "User:MDanielsBot/markAdmins-Data.js")
-combinedJsonDataPage = pywikibot.Page(site, "User:MDanielsBot/markAdmins-Data.json")
-
-
-localGroups = ["abusefilter", "abusefilter-helper", "accountcreator",
-               "bureaucrat", "checkuser", "extendedmover", "filemover",
-               "interface-admin", "massmessage-sender", "oversight",
-               "sysop", "templateeditor"]
+# localGroups = ["abusefilter", "abusefilter-helper", "accountcreator",\
+#           "autoreviewer", "bureaucrat", "checkuser", "extendedmover",\
+#           "filemover", "interface-admin", "massmessage-sender", "oversight",\
+#           "patroller", "reviewer", "rollbacker", "sysop", "templateeditor"]
+localGroups = ["abusefilter", "abusefilter-helper", "accountcreator",\
+          "bureaucrat", "checkuser", "extendedmover", "filemover",\
+          "interface-admin", "massmessage-sender", "suppress",\
+          "sysop", "templateeditor"]
 extraLocalGroups = ["autoreviewer", "patroller", "reviewer", "rollbacker"]
-globalGroups = ["otrs-member", "steward", "global-rollbacker"]
+#globalGroups = ["otrs-member" , "steward", "global-rollbacker"]
+globalGroups = ["vrt-permissions" , "steward", "global-rollbacker"]
 metaGroups = ["global-renamer"]
 arbcomJson = pywikibot.Page(site, "User:AmoryBot/crathighlighter.js/arbcom.json").get()
 arbcom_members = json.loads(arbcomJson)
 
 outputDict = {}
 
-print(datetime.now(timezone.utc).strftime("%b %d %Y %H:%M:%S.%f") +
+print(datetime.now(timezone.utc).strftime("%b %d %Y %H:%M:%S.%f") +\
       " -- Starting!", flush=True)
 
 for group in localGroups:
@@ -107,7 +94,7 @@ for group in globalGroups:
             outputDict[user['name']].append(group)
         else:
             outputDict[user['name']] = [group]
-
+            
 for user in arbcom_members:
     if user in outputDict.keys():
         outputDict[user].append("arbcom")
@@ -128,7 +115,7 @@ for group in metaGroups:
         else:
             outputDict[user['name']] = [group]
 
-print(datetime.now(timezone.utc).strftime("%b %d %Y %H:%M:%S.%f") +
+print(datetime.now(timezone.utc).strftime("%b %d %Y %H:%M:%S.%f") +\
       " -- Computing output...", flush=True)
 
 # Sort our flags
@@ -137,19 +124,19 @@ for item in outputDict:
 
 # Construct combined JSON page
 pageTop = "mw.hook('userjs.script-loaded.markadmins').fire("
-outputJson = json.dumps(outputDict, sort_keys=True,
-                        indent=4, separators=(',', ': '), ensure_ascii=False)
+outputJson = json.dumps(outputDict, sort_keys=True,\
+                     indent=4, separators=(',', ': '), ensure_ascii=False)
 pageBottom = ");"
 
-newText = pageTop + outputJson + pageBottom
+newText = pageTop + outputJson + pageBottom;
 oldJspage = combinedJsDataPage.get()
 oldJsonpage = combinedJsonDataPage.get()
-
-if newText != oldJspage or outputJson != oldJsonpage:
-    print(datetime.now(timezone.utc).strftime("%b %d %Y %H:%M:%S.%f")
-          + " -- Updated!", flush=True)
+    
+if (newText != oldJspage or outputJson != oldJsonpage):
+    print(datetime.now(timezone.utc).strftime("%b %d %Y %H:%M:%S.%f")\
+             + " -- Updated!", flush=True)
     combinedJsDataPage.put(newText, "Update markadmins data")
     combinedJsonDataPage.put(outputJson, "Update markadmins data")
 else:
-    print(datetime.now(timezone.utc).strftime("%b %d %Y %H:%M:%S.%f")
-          + " -- No changes", flush=True)
+    print(datetime.now(timezone.utc).strftime("%b %d %Y %H:%M:%S.%f")\
+             + " -- No changes", flush=True);
